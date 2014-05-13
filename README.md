29ad1880da8f49977b623fa2379fe97dc5e010d2
kodam <hotsoup.h@gmail.com>
2014-05-14 01:45:48 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Garoon::Tools - It's new $module
+
+# SYNOPSIS
+
+    use Garoon::Tools;
+
+# DESCRIPTION
+
+Garoon::Tools is ...
+
+# LICENSE
+
+Copyright (C) kodam.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+kodam <hotsoup.h@gmail.com>
---
+++ lib/Garoon/Tools.pm
@@ -0,0 +1,39 @@
+package Garoon::Tools;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Garoon::Tools - It's new $module
+
+=head1 SYNOPSIS
+
+    use Garoon::Tools;
+
+=head1 DESCRIPTION
+
+Garoon::Tools is ...
+
+=head1 LICENSE
+
+Copyright (C) kodam.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+kodam E<lt>hotsoup.h@gmail.comE<gt>
+
+=cut
+
